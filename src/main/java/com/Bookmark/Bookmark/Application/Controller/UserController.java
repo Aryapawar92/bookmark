c42c360e971849f2836d23eeee000f2df508d05e
Arya Pawar <arya.pawar@iide.co>
2025-10-15 12:20:10 +0530

    User CRUD Done.

--- src/main/java/com/Bookmark/Bookmark/Application/Controller/UserController.java
+++ src/main/java/com/Bookmark/Bookmark/Application/Controller/UserController.java
@@ -1,8 +1,10 @@
 package com.Bookmark.Bookmark.Application.Controller;
 
+import com.Bookmark.Bookmark.Application.Entity.BookmarkEntity;
 import com.Bookmark.Bookmark.Application.Entity.UserEntity;
 import com.Bookmark.Bookmark.Application.Service.UserService;
 import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,5 +29,27 @@ public class UserController {
     public ResponseEntity<List<UserEntity>> getAllUser(){
         List<UserEntity> recordsList = userService.getAllUser();
         return ResponseEntity.ok(recordsList);
+    }
+
+    @GetMapping("/search/{id}")
+    public ResponseEntity<UserEntity> getUserById(@PathVariable Long id){
+        return userService.getUserById(id).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/delete/{id}")
+    public ResponseEntity<UserEntity> deleteUser(@PathVariable Long id){
+        userService.deleteUserById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<UserEntity> updateUser(@PathVariable Long id,@RequestBody UserEntity entry){
+        UserEntity updated =  userService.updateUser(id,entry);
+
+        if (updated != null) {
+            return ResponseEntity.ok(updated);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
